526363a94f9267ae2d21f1acd7f44b498cc6e73d
KaptenenK <koosha.yousefi@hotmail.com>
2023-02-02 12:59:57 +0100

    kan nu lägga till användare.

--- VOD.Common/HttpClients/UserHttpClient.cs
+++ VOD.Common/HttpClients/UserHttpClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VOD.Common.Classes;
 
 namespace VOD.Common.HttpClients
 {
@@ -17,14 +18,49 @@ namespace VOD.Common.HttpClients
 
         public async Task CreateUser(CreateUserModel model)
         {
+             try
+        {
+            if (model == null) throw new ArgumentException("CreateUserModel is null.");
+
+            var roles = new List<string> { UserRole.Registered };
+            if (model.IsCustomer) roles.Add(UserRole.Customer);
+            if (model.IsAdmin) roles.Add(UserRole.Admin);
+
+            var user = new RegisterUserDTO(model.Email, model.Password, roles);
+
+            using StringContent jsonContent = new(
+                    JsonSerializer.Serialize(user),
+                    Encoding.UTF8,
+                    "application/json");
+
+            using HttpResponseMessage response = await Client.PostAsync("users/register", jsonContent);
+            if (!response.IsSuccessStatusCode) throw new Exception(response.ReasonPhrase);
+        }
+        catch
+        {
+            throw;
+        }
+        }
+
+        public async Task PayingCustomer(PaidCustomerDTO model)
+        {
             try
             {
-                if(model is null) throw new ArgumentExcpetion("CreateUserModel is null");
+                if (model is null) throw new ArgumentExcpetion("PaidCustomerDTO is null");
+                
+
+                using StringContent jsonContent = new(
+                         JsonSerializer.Serialize(model),
+                         Encoding.UTF8,
+                         "application/json");
+
+                using HttpResponseMessage response = await Client.PostAsync("users/paid", jsonContent);
+                if (!response.IsSuccessStatusCode) throw new Exception(response.ReasonPhrase);
             }
 
-            catch 
+            catch (Exception ex)
             {
-
+                throw;
             }
         }
     }
